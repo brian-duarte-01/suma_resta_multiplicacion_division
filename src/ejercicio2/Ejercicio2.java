790dba8bf1b7b255f126eaebfe747086412331ff
brian0106 <56491425+brian0106@users.noreply.github.com>
2021-04-20 15:35:44 -0600

    version 1.0

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,41 @@
+
+package ejercicio2;
+
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+       //declaracion de variables
+        float n1;
+        float n2;
+        float suma;
+        float resta;
+        float multiplicacion;
+        double division;
+        float divisionEntera;
+        double residuoDivision;
+        // entrada
+        n1=25;
+        n2=56;
+        //operaciones
+        suma=n1+n2;
+        resta=n1-n2;
+        multiplicacion=n1*n2;
+        division=n1/n2;
+        divisionEntera=n1/n2;
+        residuoDivision=n1%n2;
+        //salida
+        System.out.println("la suma es: "+suma);
+        System.out.println("la resta es: "+resta);
+        System.out.println("la multiplicacion es: "+multiplicacion);
+        System.out.println("la division es: "+division);
+        System.out.println("la division entera es: "+ divisionEntera);
+        System.out.println("el residuo es: "+residuoDivision);
+        
+        
+        
+        
+        
+    }
+    
+}
